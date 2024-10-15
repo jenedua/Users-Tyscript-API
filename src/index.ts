b82cec41e382e21e29357a949573384fbcef2eb0
Fedner <91825533+jenedua@users.noreply.github.com>
2024-10-15 07:26:30 -0300

    feat: inicial commit basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World3!");
+import express from 'express';
+import {config} from "dotenv";
+
+config()
+const app = express();
+
+const port =process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
