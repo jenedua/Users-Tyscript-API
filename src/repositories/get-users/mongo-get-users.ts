93ebe2118bcd5d37d87efaefbc7d67e0275b5829
Fedner <91825533+jenedua@users.noreply.github.com>
2024-10-16 07:07:39 -0300

    feat: ensure getUserMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,16 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
     async getUsers(): Promise<User[]> {
-        return  [{
-            firstname: 'Fedner',
-            lastname: 'Dabady',
-            email: 'fedner@gmail.com',
-            password: '123456'
+        const users = await MongoClient.db
+        .collection<Omit<User,"id">>('users')
+        .find({})
+        .toArray();
 
-        },
-    ];
+       
+
+        return users.map(({_id, ...rest}) => ({ ...rest, id: _id.toHexString()}));
     }
 
 }
