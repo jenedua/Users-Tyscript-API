224c49d08cb4c05de06a9558d3f7ba7da7182ed4
Fedner <91825533+jenedua@users.noreply.github.com>
2024-10-16 07:38:27 -0300

    feat: erro class MongoCreate

---
+++ src/repositories/create-users/mongo-create-user.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "../../database/mongo";
+import { User } from "../../models/users";
+import { CreateUserParams, ICreateUsersRepository } from "../../controllers/create-users/protocols";
+
+export class MongoCreateUser implements ICreateUsersRepository {
+    async createUser(params: CreateUserParams): Promise<User> {
+        
+        const { insertedId } = await MongoClient.db
+        .collection('users')
+        .insertOne(params);
+
+        const user = await MongoClient.db
+        .collection<Omit<User, 'id'>>('users')
+        .findOne({ _id: insertedId });
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        
+
+        const  { _id, ...rest } = user;
+
+        return { id: _id.toHexString(), ...rest };
+        
+        
+    }
+
+}
