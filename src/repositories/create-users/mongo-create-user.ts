4942446a889bf65a4cb8e7cf79b025ba5a132c21
Fedner <91825533+jenedua@users.noreply.github.com>
2024-10-21 10:36:16 -0300

    feat:add MongoUser Protocols

--- src/repositories/create-users/mongo-create-user.ts
+++ src/repositories/create-users/mongo-create-user.ts
@@ -1,30 +1,28 @@
 import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
-import { CreateUserParams, ICreateUserRepository } from "../../controllers/create-users/protocols";
+import {
+  CreateUserParams,
+  ICreateUserRepository,
+} from "../../controllers/create-users/protocols";
+import { MongoUser } from "../mongo-protocols";
 
 //export class MongoCreateUserRepository implements ICreateUsersRepository {
 export class MongoCreateUserRepository implements ICreateUserRepository {
-    async createUser(params: CreateUserParams): Promise<User> {
-        
-        const { insertedId } = await MongoClient.db
-        .collection('users')
-        .insertOne(params);
+  async createUser(params: CreateUserParams): Promise<User> {
+    const { insertedId } = await MongoClient.db
+      .collection("users")
+      .insertOne(params);
 
-        const user = await MongoClient.db
-        .collection<Omit<User, 'id'>>('users')
-        .findOne({ _id: insertedId });
+    const user = await MongoClient.db
+      .collection<MongoUser>("users")
+      .findOne({ _id: insertedId });
 
-        if (!user) {
-            throw new Error('User not found');
-        }
-
-        
-
-        const  { _id, ...rest } = user;
-
-        return { id: _id.toHexString(), ...rest };
-        
-        
+    if (!user) {
+      throw new Error("User not found");
     }
 
+    const { _id, ...rest } = user;
+
+    return { id: _id.toHexString(), ...rest };
+  }
 }
